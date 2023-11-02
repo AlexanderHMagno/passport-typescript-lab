cca4f8b2ab83873d178f57cacc49de204d2b9390
Alexander Hortua <alexander.hortua10@gmail.com>
2023-11-02 13:56:10 -0700

    Fixing issue with Revoking session Endpoint

--- routes/adminRoute.ts
+++ routes/adminRoute.ts
@@ -4,25 +4,29 @@ import { ensureAdmin } from "../middleware/checkAuth";
 const router = express.Router();
 
 router.get("/", ensureAdmin, (req, res) => {
-  //Get current sessions
-  //@ts-ignore
-  const currentSessions = Object.entries(req.sessionStore.sessions);
+  if (req.sessionStore.all) {
+    //create sessionBody
+    const sessionBody = {};
+    req.sessionStore.all((err, sessions) => {
+      if (sessions) {
+        const activeSessions = Array.from(Object.entries(sessions));
 
-  //create sessionBody
-  const sessionBody = {};
+        activeSessions.forEach((element) => {
+          const [keys, values] = element;
+          //@ts-ignore
+          sessionBody[keys] = values.passport?.user;
+        });
+      }
 
-  currentSessions.forEach((element) => {
-    //@ts-ignore
-    sessionBody["" + element[0]] = JSON.parse(element[1]).passport?.user;
-  });
-
-  res.render("admin", {
-    user: req.user,
-    sessions: sessionBody,
-  });
+      return res.render("admin", {
+        user: req.user,
+        sessions: sessionBody,
+      });
+    });
+  }
 });
 
-router.get("/admin/revoke/:data", ensureAdmin, (req, res) => {
+router.get("/revoke/:data", ensureAdmin, (req, res) => {
   req.sessionStore.destroy(req.params.data);
   res.redirect("/admin");
 });
